f088b06f9a20e440182ad4b341d3d486c99d1c5e
Tom Noonan II <tom@tjnii.com>
2019-10-02 19:59:13 -0400

    Old changes: Add Stretch

--- build.sh
+++ build.sh
@@ -3,5 +3,6 @@
 PREFIX=tjnii_base
 
 for base in $(find ./ -maxdepth 2 -type f -name Dockerfile | cut -f 2 -d /); do
+    grep FROM $base/Dockerfile | awk '{print $2}' | xargs docker pull
     docker build --no-cache -t ${PREFIX}/${base} ${base}
 done
