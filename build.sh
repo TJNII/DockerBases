45426178ec739b27839f9e028722e16179fb374e
Tom Noonan II <tom@tjnii.com>
2020-03-05 19:11:18 -0500

    Build in sort order

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 PREFIX=tjnii_base
 
-for base in $(find ./ -maxdepth 2 -type f -name Dockerfile | cut -f 2 -d /); do
+for base in $(find ./ -maxdepth 2 -type f -name Dockerfile | cut -f 2 -d / | sort); do
     grep FROM $base/Dockerfile | awk '{print $2}' | xargs docker pull
     docker build --no-cache -t ${PREFIX}/${base} ${base}
 done
